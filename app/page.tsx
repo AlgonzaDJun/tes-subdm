eccb5d6cd81c0656bd06ec5a04d6fd94b0e5bf16
AlgonzaDJun <algonsadewangga@gmail.com>
2024-09-19 18:42:05 +0700

    asfd

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import Image from "next/image";
 
 export default function Home() {
+  // if sushi is a subdomain, redirect to sushi page
+  if (typeof window !== "undefined") {
+    const hostname = window.location.hostname;
+    const subdomain = hostname.split(".")[0];
+    if (subdomain === "sushi") {
+      return <div>redirecting to sushi page</div>;
+    }
+  }
+
   return <div>this is the home page</div>;
 }
